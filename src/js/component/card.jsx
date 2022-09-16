b136974ec6fb2ec395bda9d730577c06978caed9
PieroMastro <pier.andres@gmail.com>
2022-09-16 22:01:25 +0000

    Added Navbar abd Card components

---
+++ src/js/component/card.jsx
@@ -0,0 +1,12 @@
+/*import React from "react";
+
+export const Card = () => {
+    return (<div className="card" style="width: 18rem;">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione neque omnis inventore accusamus repudiandae libero?</p>
+            <a href="#" className="btn btn-primary">Find out more</a>
+        </div>
+    </div>);
+};*/
